e48e744136bcafd3ab05c98aac561d459252bc90
vinicius-dec <viniciusbellonidesouza@gmail.com>
2025-04-09 17:42:10 -0300

    Update poligrafojs

--- iw2/Bimestre1/switch_case/atividade5.js
+++ iw2/Bimestre1/switch_case/atividade5.js
@@ -1,34 +1,60 @@
-let pergunta1= true
-let pergunta2= true
-let pergunta3= true
-let pergunta4= true
-let pergunta5= true
-let pergunta6= true
-let pergunta7= true
-let pergunta8= true
-let pergunta9= true
-let pergunta10= true
+let perguntas = ["Sim","Sim","Sim","Sim","Sim","Sim","Sim","Sim","Sim","Não"]
+let qntDeSim=0 ;
 
-
-
-
-console.log("1. Telefonou para a vítima?  ");
-console.log("1. Esteve no local da ocorrencia?  ");
-console.log("1. Mora no local da ocorrencia?  ");
-console.log("1. Já trabalhou com a vitima?  ");
-console.log("1. Telefonou para a vítima?  ");
-console.log("1. Telefonou para a vítima?  ");
-console.log("1. Telefonou para a vítima?  ");
-console.log("1. Telefonou para a vítima?  ");
-console.log("1. Telefonou para a vítima?  ");
-console.log("1. Telefonou para a vítima?  ");
-
-
-switch (qntDeSim) {
-    case value:
+function Sim() {
+  
+    for (let index = 0; index < perguntas.length; index++) {
+        if(perguntas[index]=="Sim"){
+            qntDeSim++
+        }
         
-        break;
+    }
+    
+}
 
-    default:
+
+
+
+console.log("1. Telefonou para a vítima?  ");
+console.log(perguntas[0]);
+
+console.log("1. Esteve no local da ocorrencia?  ");
+console.log(perguntas[1]);
+
+console.log("1. Mora no local da ocorrencia?  ");
+console.log(perguntas[2]);
+
+console.log("1. Já trabalhou com a vitima?  ");
+console.log(perguntas[3]);
+
+console.log("1. Tem grau parentesco com a vitima?  ");
+console.log(perguntas[4]);
+
+console.log("1. Você conhace a vitima?  ");
+console.log(perguntas[5]);
+
+console.log("1. Telefonou para a vítima?  ");
+console.log(perguntas[6]);
+
+console.log("1. Telefonou para a vítima?  ");
+console.log(perguntas[7]);
+
+console.log("1. Telefonou para a vítima?  ");
+console.log(perguntas[8]);
+
+console.log("1. Telefonou para a vítima?  ");
+console.log(perguntas[9]);
+
+Sim();
+switch (true) {
+    
+    case qntDeSim<=8 && qntDeSim>=6:
+        console.log("Cumplice");
         break;
+    case qntDeSim>=9 && qntDeSim<=10:
+        console.log("Culpada");
+        break;
+    default:
+        console.log("Suspeita");
+        
 }
