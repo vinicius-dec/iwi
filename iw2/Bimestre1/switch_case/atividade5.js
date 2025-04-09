c534ed7bbde7b29fe43739114825a9f3b4aed27f
vinicius-dec <viniciusbellonidesouza@gmail.com>
2025-04-09 17:54:06 -0300

    Update atividade5.js

--- iw2/Bimestre1/switch_case/atividade5.js
+++ iw2/Bimestre1/switch_case/atividade5.js
@@ -1,9 +1,13 @@
-let perguntas = ["Sim","Sim","Sim","Sim","Sim","Sim","Sim","Sim","Sim","Não"]
+// Variavel que guarda array de respostas sim 
+let perguntas = ["Sim","Não","Não","Sim","Sim","Sim","Sim","Sim","Sim","Não"]
+// Variavel para sim
 let qntDeSim=0 ;
-
+//Função para fazer o calculo de quantos sim
 function Sim() {
-  
+
+    // Looping para contatos sim tem
     for (let index = 0; index < perguntas.length; index++) {
+        // Condicional que relaciona o array e pega o lugar da lista e observa se e sim ou não, para adicionar mais 1 ao valor da variavel qntDeSim
         if(perguntas[index]=="Sim"){
             qntDeSim++
         }
@@ -14,7 +18,7 @@ function Sim() {
 
 
 
-
+// Pega o valor do lugar  do array
 console.log("1. Telefonou para a vítima?  ");
 console.log(perguntas[0]);
 
@@ -27,7 +31,7 @@ console.log(perguntas[2]);
 console.log("1. Já trabalhou com a vitima?  ");
 console.log(perguntas[3]);
 
-console.log("1. Tem grau parentesco com a vitima?  ");
+console.log("1. Tem grau de parentesco com a vitima?  ");
 console.log(perguntas[4]);
 
 console.log("1. Você conhace a vitima?  ");
@@ -45,7 +49,9 @@ console.log(perguntas[8]);
 console.log("1. Telefonou para a vítima?  ");
 console.log(perguntas[9]);
 
+// Chama a função
 Sim();
+// Não funciona sem o true
 switch (true) {
     
     case qntDeSim<=8 && qntDeSim>=6:
@@ -55,6 +61,5 @@ switch (true) {
         console.log("Culpada");
         break;
     default:
-        console.log("Suspeita");
-        
+        console.log("Suspeita");       
 }
